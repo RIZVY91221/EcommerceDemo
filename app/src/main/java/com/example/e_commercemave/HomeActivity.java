03402959da12a4c234a75f7ced971979c87e8215
Rizvy Ahmed <rizvyahamed91221@yahoo.com>
2019-03-26 20:28:27 +0600

    updateProfile

--- app/src/main/java/com/example/e_commercemave/HomeActivity.java
+++ app/src/main/java/com/example/e_commercemave/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.e_commercemave;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +17,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.e_commercemave.Model.Products;
+import com.example.e_commercemave.Prevalent.Prevalent;
+import com.example.e_commercemave.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    private DatabaseReference ProductRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ProductRef= FirebaseDatabase.getInstance().getReference().child("Products");
+
+        Paper.init(this);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Home");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -40,10 +71,57 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView=navigationView.getHeaderView(0);
+        TextView userNameTextView=headerView.findViewById(R.id.user_profile_name);
+        CircleImageView profileImageView=headerView.findViewById(R.id.user_profile_image);
+        userNameTextView.setText(Prevalent.currentOnlineUser.getName());
+
+        Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
+
+        recyclerView=(RecyclerView)findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
     @Override
-    public void onBackPressed() {
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products>options=new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductRef,Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter=
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model)
+                    {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price:"+model.getPrice()+"$");
+
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item_layout,parent,false);
+                        ProductViewHolder holder=new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
@@ -53,45 +131,56 @@ public class HomeActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
 
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_cart)
+        {
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_order)
+        {
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_category)
+        {
 
-        } else if (id == R.id.nav_share) {
+        }
+        else if (id == R.id.nav_settings)
+        {
+            Intent intent=new Intent(HomeActivity.this,SettingActivity.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_send) {
-
+        }
+        else if (id == R.id.nav_logout)
+        {
+            Paper.book().destroy();
+            Intent intent =new Intent(HomeActivity.this,MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/e_commercemave/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.e_commercemave.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.e_commercemave.Interface.ItemClickListner;
+import com.example.e_commercemave.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView  txtProductName,txtProductDescription,txtProductPrice;
+    public ImageView imageView;
+    public ItemClickListner listner;
+
+    public ProductViewHolder(View itemView)
+    {
+        super(itemView);
+
+        imageView=itemView.findViewById(R.id.product_image);
+        txtProductName=itemView.findViewById(R.id.product_name);
+        txtProductDescription=itemView.findViewById(R.id.product_description);
+        txtProductPrice=itemView.findViewById(R.id.product_price);
+
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner=listner;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view,getAdapterPosition(),false);
+    }
+}
